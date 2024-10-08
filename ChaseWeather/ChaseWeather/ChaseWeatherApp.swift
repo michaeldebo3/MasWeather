98d0f3e1b11f2de6fecb1d7f0b5bc75724cfd381
Michael de Boisblanc <michaeldebo@gmail.com>
2024-10-08 10:18:05 -0400

    initial commit

---
+++ ChaseWeather/ChaseWeather/ChaseWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChaseWeatherApp.swift
+//  ChaseWeather
+//
+//  Created by Michael deBoisblanc on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ChaseWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChaseWeather/ChaseWeatherTests/ChaseWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChaseWeatherTests.swift
+//  ChaseWeatherTests
+//
+//  Created by Michael deBoisblanc on 10/8/24.
+//
+
+import Testing
+@testable import ChaseWeather
+
+struct ChaseWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
